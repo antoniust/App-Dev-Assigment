583d391daeeec570282978c628ad9c6698c18a9c
antoniust <161410149+antoniust@users.noreply.github.com>
2024-03-19 02:52:14 +0700

    Take Home Week 4

---
+++ WindowsFormsApp6/WindowsFormsApp6/Form1.Designer.cs
@@ -0,0 +1,318 @@
+﻿namespace WindowsFormsApp6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBoxTeamPlayer = new System.Windows.Forms.ListBox();
+            this.ButtRmove = new System.Windows.Forms.Button();
+            this.labelSoccerTeamList = new System.Windows.Forms.Label();
+            this.labelChooseCountry = new System.Windows.Forms.Label();
+            this.labelChooseTeam = new System.Windows.Forms.Label();
+            this.tboxTeamCity = new System.Windows.Forms.TextBox();
+            this.tboxTeamCountry = new System.Windows.Forms.TextBox();
+            this.tboxTeamName = new System.Windows.Forms.TextBox();
+            this.labelAddingTeam = new System.Windows.Forms.Label();
+            this.labelTeamName = new System.Windows.Forms.Label();
+            this.labelTeamCountry = new System.Windows.Forms.Label();
+            this.labelTeamCity = new System.Windows.Forms.Label();
+            this.labelAddingPlayer = new System.Windows.Forms.Label();
+            this.tboxPlayerNum = new System.Windows.Forms.TextBox();
+            this.tboxPlayerName = new System.Windows.Forms.TextBox();
+            this.labelPlayerName = new System.Windows.Forms.Label();
+            this.labelPlayerNum = new System.Windows.Forms.Label();
+            this.labelPlayerPosition = new System.Windows.Forms.Label();
+            this.buttonAddTeam = new System.Windows.Forms.Button();
+            this.buttonAddPlayer = new System.Windows.Forms.Button();
+            this.comboBoxCountry = new System.Windows.Forms.ComboBox();
+            this.comboBoxTeam = new System.Windows.Forms.ComboBox();
+            this.CmboBoxPlayerPosition = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // listBoxTeamPlayer
+            // 
+            this.listBoxTeamPlayer.FormattingEnabled = true;
+            this.listBoxTeamPlayer.Location = new System.Drawing.Point(577, 45);
+            this.listBoxTeamPlayer.MultiColumn = true;
+            this.listBoxTeamPlayer.Name = "listBoxTeamPlayer";
+            this.listBoxTeamPlayer.Size = new System.Drawing.Size(211, 251);
+            this.listBoxTeamPlayer.TabIndex = 1;
+            // 
+            // ButtRmove
+            // 
+            this.ButtRmove.Location = new System.Drawing.Point(652, 330);
+            this.ButtRmove.Name = "ButtRmove";
+            this.ButtRmove.Size = new System.Drawing.Size(75, 23);
+            this.ButtRmove.TabIndex = 3;
+            this.ButtRmove.Text = "Remove";
+            this.ButtRmove.UseVisualStyleBackColor = true;
+            this.ButtRmove.Click += new System.EventHandler(this.ButtRmove_Click);
+            // 
+            // labelSoccerTeamList
+            // 
+            this.labelSoccerTeamList.AutoSize = true;
+            this.labelSoccerTeamList.Location = new System.Drawing.Point(162, 9);
+            this.labelSoccerTeamList.Name = "labelSoccerTeamList";
+            this.labelSoccerTeamList.Size = new System.Drawing.Size(90, 13);
+            this.labelSoccerTeamList.TabIndex = 4;
+            this.labelSoccerTeamList.Text = "Soccer Team List";
+            // 
+            // labelChooseCountry
+            // 
+            this.labelChooseCountry.AutoSize = true;
+            this.labelChooseCountry.Location = new System.Drawing.Point(101, 44);
+            this.labelChooseCountry.Name = "labelChooseCountry";
+            this.labelChooseCountry.Size = new System.Drawing.Size(88, 13);
+            this.labelChooseCountry.TabIndex = 5;
+            this.labelChooseCountry.Text = "Choose Country :";
+            // 
+            // labelChooseTeam
+            // 
+            this.labelChooseTeam.AutoSize = true;
+            this.labelChooseTeam.Location = new System.Drawing.Point(105, 76);
+            this.labelChooseTeam.Name = "labelChooseTeam";
+            this.labelChooseTeam.Size = new System.Drawing.Size(79, 13);
+            this.labelChooseTeam.TabIndex = 6;
+            this.labelChooseTeam.Text = "Choose Team :";
+            this.labelChooseTeam.Click += new System.EventHandler(this.labelChooseTeam_Click);
+            // 
+            // tboxTeamCity
+            // 
+            this.tboxTeamCity.Location = new System.Drawing.Point(108, 228);
+            this.tboxTeamCity.Name = "tboxTeamCity";
+            this.tboxTeamCity.Size = new System.Drawing.Size(100, 20);
+            this.tboxTeamCity.TabIndex = 8;
+            // 
+            // tboxTeamCountry
+            // 
+            this.tboxTeamCountry.Location = new System.Drawing.Point(108, 197);
+            this.tboxTeamCountry.Name = "tboxTeamCountry";
+            this.tboxTeamCountry.Size = new System.Drawing.Size(100, 20);
+            this.tboxTeamCountry.TabIndex = 9;
+            // 
+            // tboxTeamName
+            // 
+            this.tboxTeamName.Location = new System.Drawing.Point(108, 162);
+            this.tboxTeamName.Name = "tboxTeamName";
+            this.tboxTeamName.Size = new System.Drawing.Size(100, 20);
+            this.tboxTeamName.TabIndex = 10;
+            // 
+            // labelAddingTeam
+            // 
+            this.labelAddingTeam.AutoSize = true;
+            this.labelAddingTeam.Location = new System.Drawing.Point(41, 128);
+            this.labelAddingTeam.Name = "labelAddingTeam";
+            this.labelAddingTeam.Size = new System.Drawing.Size(73, 13);
+            this.labelAddingTeam.TabIndex = 11;
+            this.labelAddingTeam.Text = "Adding Team ";
+            // 
+            // labelTeamName
+            // 
+            this.labelTeamName.AutoSize = true;
+            this.labelTeamName.Location = new System.Drawing.Point(12, 165);
+            this.labelTeamName.Name = "labelTeamName";
+            this.labelTeamName.Size = new System.Drawing.Size(71, 13);
+            this.labelTeamName.TabIndex = 12;
+            this.labelTeamName.Text = "Team Name :";
+            // 
+            // labelTeamCountry
+            // 
+            this.labelTeamCountry.AutoSize = true;
+            this.labelTeamCountry.Location = new System.Drawing.Point(12, 200);
+            this.labelTeamCountry.Name = "labelTeamCountry";
+            this.labelTeamCountry.Size = new System.Drawing.Size(79, 13);
+            this.labelTeamCountry.TabIndex = 13;
+            this.labelTeamCountry.Text = "Team Country :";
+            // 
+            // labelTeamCity
+            // 
+            this.labelTeamCity.AutoSize = true;
+            this.labelTeamCity.Location = new System.Drawing.Point(12, 231);
+            this.labelTeamCity.Name = "labelTeamCity";
+            this.labelTeamCity.Size = new System.Drawing.Size(60, 13);
+            this.labelTeamCity.TabIndex = 14;
+            this.labelTeamCity.Text = "Team City :";
+            // 
+            // labelAddingPlayer
+            // 
+            this.labelAddingPlayer.AutoSize = true;
+            this.labelAddingPlayer.Location = new System.Drawing.Point(262, 128);
+            this.labelAddingPlayer.Name = "labelAddingPlayer";
+            this.labelAddingPlayer.Size = new System.Drawing.Size(78, 13);
+            this.labelAddingPlayer.TabIndex = 15;
+            this.labelAddingPlayer.Text = "Adding Player :";
+            // 
+            // tboxPlayerNum
+            // 
+            this.tboxPlayerNum.Location = new System.Drawing.Point(372, 197);
+            this.tboxPlayerNum.Name = "tboxPlayerNum";
+            this.tboxPlayerNum.Size = new System.Drawing.Size(121, 20);
+            this.tboxPlayerNum.TabIndex = 17;
+            // 
+            // tboxPlayerName
+            // 
+            this.tboxPlayerName.Location = new System.Drawing.Point(372, 162);
+            this.tboxPlayerName.Name = "tboxPlayerName";
+            this.tboxPlayerName.Size = new System.Drawing.Size(121, 20);
+            this.tboxPlayerName.TabIndex = 18;
+            // 
+            // labelPlayerName
+            // 
+            this.labelPlayerName.AutoSize = true;
+            this.labelPlayerName.Location = new System.Drawing.Point(267, 165);
+            this.labelPlayerName.Name = "labelPlayerName";
+            this.labelPlayerName.Size = new System.Drawing.Size(73, 13);
+            this.labelPlayerName.TabIndex = 19;
+            this.labelPlayerName.Text = "Player Name :";
+            // 
+            // labelPlayerNum
+            // 
+            this.labelPlayerNum.AutoSize = true;
+            this.labelPlayerNum.Location = new System.Drawing.Point(267, 200);
+            this.labelPlayerNum.Name = "labelPlayerNum";
+            this.labelPlayerNum.Size = new System.Drawing.Size(82, 13);
+            this.labelPlayerNum.TabIndex = 20;
+            this.labelPlayerNum.Text = "Player Number :";
+            // 
+            // labelPlayerPosition
+            // 
+            this.labelPlayerPosition.AutoSize = true;
+            this.labelPlayerPosition.Location = new System.Drawing.Point(267, 235);
+            this.labelPlayerPosition.Name = "labelPlayerPosition";
+            this.labelPlayerPosition.Size = new System.Drawing.Size(82, 13);
+            this.labelPlayerPosition.TabIndex = 21;
+            this.labelPlayerPosition.Text = "Player Position :";
+            // 
+            // buttonAddTeam
+            // 
+            this.buttonAddTeam.Location = new System.Drawing.Point(79, 273);
+            this.buttonAddTeam.Name = "buttonAddTeam";
+            this.buttonAddTeam.Size = new System.Drawing.Size(75, 23);
+            this.buttonAddTeam.TabIndex = 22;
+            this.buttonAddTeam.Text = "Add";
+            this.buttonAddTeam.UseVisualStyleBackColor = true;
+            this.buttonAddTeam.Click += new System.EventHandler(this.buttonAddTeam_Click);
+            // 
+            // buttonAddPlayer
+            // 
+            this.buttonAddPlayer.Location = new System.Drawing.Point(329, 273);
+            this.buttonAddPlayer.Name = "buttonAddPlayer";
+            this.buttonAddPlayer.Size = new System.Drawing.Size(75, 23);
+            this.buttonAddPlayer.TabIndex = 23;
+            this.buttonAddPlayer.Text = "Add";
+            this.buttonAddPlayer.UseVisualStyleBackColor = true;
+            this.buttonAddPlayer.Click += new System.EventHandler(this.buttonAddPlayer_Click);
+            // 
+            // comboBoxCountry
+            // 
+            this.comboBoxCountry.FormattingEnabled = true;
+            this.comboBoxCountry.Location = new System.Drawing.Point(204, 41);
+            this.comboBoxCountry.Name = "comboBoxCountry";
+            this.comboBoxCountry.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxCountry.TabIndex = 24;
+            this.comboBoxCountry.SelectedIndexChanged += new System.EventHandler(this.comboBoxCountry_SelectedIndexChanged);
+            // 
+            // comboBoxTeam
+            // 
+            this.comboBoxTeam.FormattingEnabled = true;
+            this.comboBoxTeam.Location = new System.Drawing.Point(204, 73);
+            this.comboBoxTeam.Name = "comboBoxTeam";
+            this.comboBoxTeam.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxTeam.TabIndex = 25;
+            this.comboBoxTeam.SelectedIndexChanged += new System.EventHandler(this.comboBoxTeam_SelectedIndexChanged);
+            // 
+            // CmboBoxPlayerPosition
+            // 
+            this.CmboBoxPlayerPosition.FormattingEnabled = true;
+            this.CmboBoxPlayerPosition.Location = new System.Drawing.Point(372, 228);
+            this.CmboBoxPlayerPosition.Name = "CmboBoxPlayerPosition";
+            this.CmboBoxPlayerPosition.Size = new System.Drawing.Size(121, 21);
+            this.CmboBoxPlayerPosition.TabIndex = 26;
+            this.CmboBoxPlayerPosition.SelectedIndexChanged += new System.EventHandler(this.CmboBoxPlayerPosition_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.CmboBoxPlayerPosition);
+            this.Controls.Add(this.comboBoxTeam);
+            this.Controls.Add(this.comboBoxCountry);
+            this.Controls.Add(this.buttonAddPlayer);
+            this.Controls.Add(this.buttonAddTeam);
+            this.Controls.Add(this.labelPlayerPosition);
+            this.Controls.Add(this.labelPlayerNum);
+            this.Controls.Add(this.labelPlayerName);
+            this.Controls.Add(this.tboxPlayerName);
+            this.Controls.Add(this.tboxPlayerNum);
+            this.Controls.Add(this.labelAddingPlayer);
+            this.Controls.Add(this.labelTeamCity);
+            this.Controls.Add(this.labelTeamCountry);
+            this.Controls.Add(this.labelTeamName);
+            this.Controls.Add(this.labelAddingTeam);
+            this.Controls.Add(this.tboxTeamName);
+            this.Controls.Add(this.tboxTeamCountry);
+            this.Controls.Add(this.tboxTeamCity);
+            this.Controls.Add(this.labelChooseTeam);
+            this.Controls.Add(this.labelChooseCountry);
+            this.Controls.Add(this.labelSoccerTeamList);
+            this.Controls.Add(this.ButtRmove);
+            this.Controls.Add(this.listBoxTeamPlayer);
+            this.Name = "Form1";
+            this.Text = " ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ListBox listBoxTeamPlayer;
+        private System.Windows.Forms.Button ButtRmove;
+        private System.Windows.Forms.Label labelSoccerTeamList;
+        private System.Windows.Forms.Label labelChooseCountry;
+        private System.Windows.Forms.Label labelChooseTeam;
+        private System.Windows.Forms.TextBox tboxTeamCity;
+        private System.Windows.Forms.TextBox tboxTeamCountry;
+        private System.Windows.Forms.TextBox tboxTeamName;
+        private System.Windows.Forms.Label labelAddingTeam;
+        private System.Windows.Forms.Label labelTeamName;
+        private System.Windows.Forms.Label labelTeamCountry;
+        private System.Windows.Forms.Label labelTeamCity;
+        private System.Windows.Forms.Label labelAddingPlayer;
+        private System.Windows.Forms.TextBox tboxPlayerNum;
+        private System.Windows.Forms.TextBox tboxPlayerName;
+        private System.Windows.Forms.Label labelPlayerName;
+        private System.Windows.Forms.Label labelPlayerNum;
+        private System.Windows.Forms.Label labelPlayerPosition;
+        private System.Windows.Forms.Button buttonAddTeam;
+        private System.Windows.Forms.Button buttonAddPlayer;
+        private System.Windows.Forms.ComboBox comboBoxCountry;
+        private System.Windows.Forms.ComboBox comboBoxTeam;
+        private System.Windows.Forms.ComboBox CmboBoxPlayerPosition;
+    }
+}
+
---
+++ WindowsFormsApp6/WindowsFormsApp6/Form1.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp6
+{
+    public partial class Form1 : Form
+    {
+        DataTable dtsoccer = new DataTable();
+       DataTable dtRegion = new DataTable();
+
+
+
+        private Dictionary<string, List<string>> _countriesAndClubs = new Dictionary<string, List<string>>()
+        {
+            { "England", new List<string>() { "Manchester United", "Liverpool", "Chelsea", "Arsenal" } },
+            { "Spain", new List<string>() { "Real Madrid", "Barcelona", "Atletico Madrid", "Sevilla" } },
+            { "Germany", new List<string>() { "Bayern Munich", "Borussia Dortmund", "RB Leipzig", "Bayer Leverkusen" } },
+            { "Italy", new List<string>() { "Juventus", "Inter Milan", "AC Milan", "Napoli" } }
+        };
+
+
+
+        List<string> Country = new List<string>() { "England", "Germany" , "Spain" , "Italy" };
+        List<string> England = new List<string>() { "Chelsea", "Manchester United", };
+        List<string> Germany = new List<string>() { "Bayern Munich", "Borussia Dortmund" };
+        List<string> Spain = new List<string>() { "Barcelona" };
+        List<string> Italy = new List<string>() { "Inter Milan" };
+
+        List<string> Position = new List<string>() { "GK","DF", "FW", "MF"};
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            comboBoxCountry.DataSource = Country;
+            CmboBoxPlayerPosition.DataSource = Position;
+
+            dtRegion.Columns.Add("England");
+            dtRegion.Columns.Add("Germany");
+
+            dtRegion.Rows.Add("Chelsea", "Bayern Munich");
+            dtRegion.Rows.Add("Manchester United" , "");
+           
+
+            //dtChelsea.Columns.Add("Profil");
+            dtsoccer.Columns.Add("Country");
+            dtsoccer.Columns.Add("Tim");
+            dtsoccer.Columns.Add("No");
+            dtsoccer.Columns.Add("Name");
+            dtsoccer.Columns.Add("Pos");
+            dtsoccer.Rows.Add("England", "Chelsea" ,"01"," Robert Sánchez." ,"GK");
+            dtsoccer.Rows.Add("England","Chelsea" ,"02", "Axel Disasi.", "DF");
+            dtsoccer.Rows.Add("England", "Chelsea" ,"03", "Marc Cucurella.", "DF");
+            dtsoccer.Rows.Add("England", "Chelsea" ,"05", "Benoît Badiashile.", "DF");
+            dtsoccer.Rows.Add("England", "Chelsea" , "07", "Raheem Sterling.", "FW");
+            dtsoccer.Rows.Add("England", "Chelsea" ,"13", "Marcus Bettinelli.", "GK");
+            dtsoccer.Rows.Add("England", "Chelsea" ,"20", " Cole Palmer.", "MF");
+            dtsoccer.Rows.Add("England", "Chelsea" ,"08", "Enzo Fernández.", "MF");
+            dtsoccer.Rows.Add("England", "Chelsea" ,"18", "Christopher Nkunku.", "FW");
+            dtsoccer.Rows.Add("England", "Chelsea" ,"43", "Diego Moreira.", "FW");
+            dtsoccer.Rows.Add("England", "Chelsea" ,"28", "Đorđe Petrović.", "GK");
+
+            dtsoccer.Rows.Add("England", "Manchester United", "17", "Alejandro Garnacho.", "FW");
+            dtsoccer.Rows.Add("England", "Manchester United", "01", "Altay Bayındır.", "GK");
+            dtsoccer.Rows.Add("England", "Manchester United", "02", "Victor Lindelöf.", "DF");
+            dtsoccer.Rows.Add("England", "Manchester United", "04", "Sofyan Amrabat.", "MF");
+            dtsoccer.Rows.Add("England", "Manchester United", "05", "Harry Maguire.", "DF");
+            dtsoccer.Rows.Add("England", "Manchester United", "08", "Bruno Fernandes.", "MF");
+            dtsoccer.Rows.Add("England", "Manchester United", "09", "Anthony Martial.", "FW");
+            dtsoccer.Rows.Add("England", "Manchester United", "23", "Luke Shaw", "DK");
+            dtsoccer.Rows.Add("England", "Manchester United", "24", "André Onana", "GK");
+            dtsoccer.Rows.Add("England", "Manchester United", "14", "Christian Eriksen", "MF");
+            dtsoccer.Rows.Add("England", "Manchester United", "47", "Shola Shoretire", "MF");
+
+
+            dtsoccer.Rows.Add("Germany", "Bayern Munich", "09", "Harry Kane", "FW");
+            dtsoccer.Rows.Add("Germany", "Bayern Munich", "04", "Matthijs de Ligt", "DF");
+            dtsoccer.Rows.Add("Germany", "Bayern Munich", "22", "Raphaël Guerreiro", "DF");
+            dtsoccer.Rows.Add("Germany", "Bayern Munich", "07", "Serge Gnabry", "FW");
+            dtsoccer.Rows.Add("Germany", "Bayern Munich", "18", "Daniel Peretz", "GK");
+            dtsoccer.Rows.Add("Germany", "Bayern Munich", "42", "Jamal Musiala", "MF");
+            dtsoccer.Rows.Add("Germany", "Bayern Munich", "03", "Kim Min-jae", "DF");
+            dtsoccer.Rows.Add("Germany", "Bayern Munich", "01", "Manuel Neuer", "GK");
+            dtsoccer.Rows.Add("Germany", "Bayern Munich", "27", "Konrad Laimer", "MF");
+            dtsoccer.Rows.Add("Germany", "Bayern Munich", "28", "Tarek Buchmann", "DF");
+            dtsoccer.Rows.Add("Germany", "Bayern Munich", "13", "Eric M. Choupo-Moting", "FW");
+
+            dtsoccer.Rows.Add("Germany", "Borussia Dortmund", "01", "Gregor Kobel.", "GK");
+            dtsoccer.Rows.Add("Germany", "Borussia Dortmund", "17", "Marius Wolf.", "MF");
+            dtsoccer.Rows.Add("Germany", "Borussia Dortmund", "08", "Felix Nmecha.", "MF");
+            dtsoccer.Rows.Add("Germany", "Borussia Dortmund", "26", "Julian Ryerson.", "DF");
+            dtsoccer.Rows.Add("Germany", "Borussia Dortmund", "33", "Alexander Meyer.", "GK");
+            dtsoccer.Rows.Add("Germany", "Borussia Dortmund", "02", "Mateu Morey.", "DF");
+            dtsoccer.Rows.Add("Germany", "Borussia Dortmund", "19", "Julian Brandt.", "MF");
+            dtsoccer.Rows.Add("Germany", "Borussia Dortmund", "14", "Niclas Füllkrug.", "FW");
+            dtsoccer.Rows.Add("Germany", "Borussia Dortmund", "43", "Jamie Bynoe-Gittens.", "FW");
+            dtsoccer.Rows.Add("Germany", "Borussia Dortmund", "21", "Donyell Malen.", "FW");
+            dtsoccer.Rows.Add("Germany", "Borussia Dortmund", "15", "Mats Hummels", "DF");
+
+            dtsoccer.Rows.Add("Italy", "Inter Milan", "02", "Denzel Dumfries.", "MF");
+            dtsoccer.Rows.Add("Italy", "Inter Milan", "07", "Juan Cuadrado.", "MF");
+            dtsoccer.Rows.Add("Italy", "Inter Milan", "31", "Yann Bisseck.", "DF");
+            dtsoccer.Rows.Add("Italy", "Inter Milan", "70", "Alexis Sánchez.", "FW");
+            dtsoccer.Rows.Add("Italy", "Inter Milan", "14", "Davy Klaassen.", "MF");
+            dtsoccer.Rows.Add("Italy", "Inter Milan", "01", "Yann Sommer.", "GK");
+            dtsoccer.Rows.Add("Italy", "Inter Milan", "15", "Francesco Acerbi.", "DF");
+            dtsoccer.Rows.Add("Italy", "Inter Milan", "32", "Federico Dimarco.", "DF");
+            dtsoccer.Rows.Add("Italy", "Inter Milan", "10", "Lautaro Martínez.", "FW");
+            dtsoccer.Rows.Add("Italy", "Inter Milan", "09", "Marcus Thuram.", "FW");
+            dtsoccer.Rows.Add("Italy", "Inter Milan", "77", "Emil Audero.", "GK");
+
+            dtsoccer.Rows.Add("Spain", "Barcelona", "04", "Ronald Araújo.", "DF");
+            dtsoccer.Rows.Add("Spain", "Barcelona", "08", "Pedri.", "MF");
+            dtsoccer.Rows.Add("Spain", "Barcelona", "15", "Andreas Christensen.", "DF");
+            dtsoccer.Rows.Add("Spain", "Barcelona", "01", "Marc André ter Stegen.", "GK");
+            dtsoccer.Rows.Add("Spain", "Barcelona", "16", "Fermín López.", "MF");
+            dtsoccer.Rows.Add("Spain", "Barcelona", "19", "Vitor Roque.", "FW");
+            dtsoccer.Rows.Add("Spain", "Barcelona", "06", "Gavi.", "MF");
+            dtsoccer.Rows.Add("Spain", "Barcelona", "02", "João Cancelo.", "DF");
+            dtsoccer.Rows.Add("Spain", "Barcelona", "09", "Robert Lewandowski.", "FW");
+            dtsoccer.Rows.Add("Spain", "Barcelona", "13", "Iñaki Peña.", "GK");
+            dtsoccer.Rows.Add("Spain", "Barcelona", "18", "Oriol Romeu.", "MF");
+            //dtsoccer.Rows.Add("Spain", "Barcelona", "", "", "");
+
+
+
+
+
+
+
+
+
+
+        }
+
+        private void labelChooseTeam_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBoxCountry_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBoxCountry.SelectedItem == "England")
+            {
+
+
+                comboBoxTeam.DataSource = England;
+
+            }
+            else if (comboBoxCountry.SelectedItem == "Germany")
+            {
+
+                comboBoxTeam.DataSource = Germany;
+
+            }
+            else if (comboBoxCountry.SelectedItem == "Spain")
+            {
+                comboBoxTeam.DataSource = Spain;
+            }
+            else if (comboBoxCountry.SelectedItem == "Italy")
+            {
+                comboBoxTeam.DataSource = Italy;
+
+
+            }
+        }
+
+        private void comboBoxTeam_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            listBoxTeamPlayer.Items.Clear();
+                for (int i = 0;i < dtsoccer.Rows.Count; i++) 
+                {
+                    if (dtsoccer.Rows[i][1] == comboBoxTeam.SelectedItem) 
+                    {
+                    listBoxTeamPlayer.Items.Add("(" + dtsoccer.Rows[i][2] + ")" + " " + dtsoccer.Rows[i][3].ToString() + " " +dtsoccer.Rows[i][4].ToString());
+                    }
+                    
+                }
+
+
+            
+        }
+
+        
+
+        private void buttonAddTeam_Click(object sender, EventArgs e)
+        {
+            Country.Add(tboxTeamCountry.Text);
+
+           
+        }
+
+        private void ButtRmove_Click(object sender, EventArgs e)
+        {
+            if (listBoxTeamPlayer.Items.Count > 11) 
+            { 
+                listBoxTeamPlayer.Items.Remove(listBoxTeamPlayer.SelectedItem); 
+            }else { MessageBox.Show("Anda bisa mengurangi pemain apabila pemain lebih dari 11 Pemain."); }
+           
+        }
+
+        private void buttonAddPlayer_Click(object sender, EventArgs e)
+        {
+            listBoxTeamPlayer.Items.Add("(" + tboxPlayerNum.Text + ")" +" "+ tboxPlayerName.Text+"." + CmboBoxPlayerPosition.SelectedItem);
+            //dtsoccer.Rows.Add("","",tboxPlayerNum.Text,tboxPlayerName.Text,CmboBoxPlayerPosition.SelectedItem);
+        }
+
+        private void CmboBoxPlayerPosition_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
