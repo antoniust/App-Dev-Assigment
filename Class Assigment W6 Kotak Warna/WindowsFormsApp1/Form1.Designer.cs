72066bc339a874e87db2a59ae33050c8cc7da323
antoniust <161410149+antoniust@users.noreply.github.com>
2024-03-26 13:08:51 +0700

    Add files via upload

---
+++ Class Assigment W6 Kotak Warna/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxUKURAN = new System.Windows.Forms.TextBox();
+            this.panelMAIN = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonSTART = new System.Windows.Forms.Button();
+            this.panelMAIN.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBoxUKURAN
+            // 
+            this.textBoxUKURAN.Location = new System.Drawing.Point(386, 141);
+            this.textBoxUKURAN.Name = "textBoxUKURAN";
+            this.textBoxUKURAN.Size = new System.Drawing.Size(100, 20);
+            this.textBoxUKURAN.TabIndex = 1;
+            this.textBoxUKURAN.TextChanged += new System.EventHandler(this.textBoxUKURAN_TextChanged);
+            // 
+            // panelMAIN
+            // 
+            this.panelMAIN.Controls.Add(this.buttonSTART);
+            this.panelMAIN.Controls.Add(this.label1);
+            this.panelMAIN.Controls.Add(this.textBoxUKURAN);
+            this.panelMAIN.Location = new System.Drawing.Point(12, 12);
+            this.panelMAIN.Name = "panelMAIN";
+            this.panelMAIN.Size = new System.Drawing.Size(786, 426);
+            this.panelMAIN.TabIndex = 2;
+            this.panelMAIN.Paint += new System.Windows.Forms.PaintEventHandler(this.panelMAIN_Paint);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(288, 144);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Masukan Ukuran ";
+            // 
+            // buttonSTART
+            // 
+            this.buttonSTART.Location = new System.Drawing.Point(350, 189);
+            this.buttonSTART.Name = "buttonSTART";
+            this.buttonSTART.Size = new System.Drawing.Size(75, 23);
+            this.buttonSTART.TabIndex = 4;
+            this.buttonSTART.Text = "START";
+            this.buttonSTART.UseVisualStyleBackColor = true;
+            this.buttonSTART.Click += new System.EventHandler(this.buttonSTART_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panelMAIN);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panelMAIN.ResumeLayout(false);
+            this.panelMAIN.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBoxUKURAN;
+        private System.Windows.Forms.Panel panelMAIN;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonSTART;
+    }
+}
+
---
+++ Class Assigment W6 Kotak Warna/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        List<Button> buttonList = new List<Button>();
+        List<Label> labelListscore = new List<Label>();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxUKURAN_TextChanged(object sender, EventArgs e)
+        {
+
+
+
+
+
+        }
+
+        private void panelMAIN_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        int size = 0;
+        private void buttonSTART_Click(object sender, EventArgs e)
+        {
+            size = Convert.ToInt32(textBoxUKURAN.Text);
+            Button[,] butnLoc = new Button[size, size];
+            panelMAIN.Visible = true;
+            if (size % 2 == 0)
+            {
+                MessageBox.Show("Input Ukuran harus Ganjil ");
+            }
+            else
+            {
+                panelMAIN.Visible = false;
+            }
+
+
+
+
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    Button buttonBaru = new Button();
+                    //buttonBaru.Text = i.ToString() + "," + j.ToString();
+                    //buttonBaru.Name = i.ToString() + "," + j.ToString();
+                    buttonBaru.Size = new Size(40, 40);
+                    buttonBaru.Location = new Point(j * 40, i * 40);
+                    buttonBaru.Tag = "White";
+                    this.Controls.Add(buttonBaru);
+                    butnLoc[i, j] = buttonBaru;
+                    buttonBaru.Click += new EventHandler(buttonClicksEvent);
+                    buttonList.Add(buttonBaru);
+
+
+                }
+            }
+
+
+
+
+
+
+
+
+
+
+        }
+
+        public void buttonClicksEvent(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            Point btnLocation = btn.Location;
+            int posX = btn.Location.X;
+            int posY = btn.Location.Y;
+            for (int i = 0; i < buttonList.Count; i++)
+            {
+                if (buttonList[i].Location.X == posX && buttonList[i].Location.Y == posY - 40 && buttonList[i].Tag != "White")
+                {
+                    if (buttonList[i].Tag == "red")
+                    {
+                        buttonList[i].BackColor = Color.Blue;
+                        buttonList[i].Tag = "blue";
+                    }
+                    else if (buttonList[i].Tag == "blue")
+                    {
+                        buttonList[i].BackColor = Color.Red;
+                        buttonList[i].Tag = "red";
+                    }
+                }
+                    if (buttonList[i].Location.X == posX && buttonList[i].Location.Y == posY + 40 && buttonList[i].Tag != "White")
+                    {
+                        if (buttonList[i].Tag == "red")
+                        {
+                            buttonList[i].BackColor = Color.Blue;
+                            buttonList[i].Tag = "blue";
+                        }
+                        else if (buttonList[i].Tag == "blue")
+                        {
+                            buttonList[i].BackColor = Color.Red;
+                            buttonList[i].Tag = "red";
+                        }
+                    }
+                    if (buttonList[i].Location.X == posX - 40 && buttonList[i].Location.Y == posY && buttonList[i].Tag != "White")
+                    {
+                        if (buttonList[i].Tag == "red")
+                        {
+                            buttonList[i].BackColor = Color.Blue;
+                            buttonList[i].Tag = "blue";
+                        }
+                        else if (buttonList[i].Tag == "blue")
+                        {
+                            buttonList[i].BackColor = Color.Red;
+                            buttonList[i].Tag = "red";
+                        }
+                    }
+                    if (buttonList[i].Location.X == posX + 40 && buttonList[i].Location.Y == posY && buttonList[i].Tag != "White")
+                    {
+                        if (buttonList[i].Tag == "red")
+                        {
+                            buttonList[i].BackColor = Color.Blue;
+                            buttonList[i].Tag = "blue";
+                        }
+                        else if (buttonList[i].Tag == "blue")
+                        {
+                            buttonList[i].BackColor = Color.Red;
+                            buttonList[i].Tag = "red";
+                        }
+                    }
+                    
+
+
+
+                
+                if (btn.Tag == "White")
+                {
+                    btn.BackColor = Color.Red;
+                    btn.Tag = "red";
+
+                }
+
+                int scoreRED = 0;
+                int scoreBlue = 0;
+
+                foreach(Button button in buttonList) 
+                { 
+                    if (button.Tag == "red") {  scoreRED++; }
+                    if (btn.Tag == "blue") { scoreBlue++; }
+                }
+                if (scoreRED == (size * size)) 
+                {
+                    MessageBox.Show("You Win");
+                }
+                if (scoreBlue == (size * size))
+                {
+                    MessageBox.Show("You Win");
+                }
+                
+
+
+            }
+        }
+    }
+}
